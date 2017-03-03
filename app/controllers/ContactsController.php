c0f959129f8b7498540df5360336f6e335bcb1de
Kongyot Rojphutthikul <kongtoonarmy@Kongyots-MacBook-Pro.local>
2017-03-03 08:41:33 +0700

    Created Bootstrap file and start with indexController

---
+++ app/controllers/ContactsController.php
@@ -0,0 +1,18 @@
+<?php
+
+use Phalcon\Mvc\Controller;
+
+class ContactsController extends Controller {
+
+    public function indexAction()
+    {
+        // $this->view->setParamToView('title', "My Contacts");
+        // $this->view->title = "My Contacts";
+        //$this->view->setVar('title', "My Contacts");
+
+        $this->view->setVars([
+            'title' => "My Contacts",
+            'count' => "0000"
+        ]);
+    }
+}
---
+++ public/index.php
@@ -0,0 +1,41 @@
+<?php
+
+use Phalcon\Loader;
+use Phalcon\Di\FactoryDefault;
+use Phalcon\Mvc\Application;
+use Phalcon\Mvc\View;
+use Phalcon\Mvc\Url as UrlProvider;
+
+try {
+    // Register the loader component
+    $loader = new Loader();
+    $loader->registerDirs([
+        '../app/controllers/',
+        '../app/models/'
+    ]);
+    $loader->register();
+
+    // Di Container
+    $di = new FactoryDefault();
+
+    // Register the view service
+    $di->set('view', function() {
+        $view = new View();
+        $view->setViewsDir('../app/views');
+        return $view;
+    });
+
+    // Register the URL service
+    $di->set("url", function () {
+        $url = new UrlProvider();
+        $url->setBaseUri("/phonebook/");
+        return $url;
+    });
+
+    // handle requests
+    $app = new Application($di);
+    echo $app->handle()->getContent();
+
+} catch (\Exception $e) {
+    echo "Exception ", $e->getMessage();
+}
