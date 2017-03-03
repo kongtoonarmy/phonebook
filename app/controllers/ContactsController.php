d691fdcb616a7170102e4ff2d618cc73b22ace57
kongtoonarmy <kongtoonarmy@hotmail.com>
2017-03-03 09:33:55 +0700

    Created Contacts model and fetching data

--- app/controllers/ContactsController.php
+++ app/controllers/ContactsController.php
@@ -6,13 +6,7 @@ class ContactsController extends Controller {
 
     public function indexAction()
     {
-        // $this->view->setParamToView('title', "My Contacts");
-        // $this->view->title = "My Contacts";
-        //$this->view->setVar('title', "My Contacts");
-
-        $this->view->setVars([
-            'title' => "My Contacts...",
-            'count' => "0000"
-        ]);
+        $this->view->contacts = Contacts::find();
+        $this->view->title = "My Contacts";
     }
 }
