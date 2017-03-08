432eb723f1bcf42cbd7385895118034ce43f0f32
kongtoonarmy <kongtoonarmy@hotmail.com>
2017-03-08 10:06:28 +0700

    Adding New Contacts

--- app/controllers/ContactsController.php
+++ app/controllers/ContactsController.php
@@ -10,6 +10,46 @@ class ContactsController extends Controller {
         $this->view->title = "My Contacts";
     }
 
+    // For rendering the new contact form
+    public function newAction()
+    {
+
+    }
+
+    // For creating a new contact in db
+    public function createAction()
+    {
+        $this->request->getPost('name');
+        $contact = new Contacts();
+        /*$contact->name = $this->request->getPost('name');
+        $contact->phone = $this->request->getPost('phone');
+        $contact->email = $this->request->getPost('email');
+        $success = $contact->save();*/
+
+        // Another way
+        $success = $contact->save($this->request->getPost(), [
+            'name', 'phone', 'email'
+        ]);
+
+        if ($success) {
+            $this->flash->success("Contact Successfully saved");
+            $this->dispatcher->forward([
+                'action' => 'index'
+            ]);
+        } else {
+            $this->flash->error("Following Errors occurred: <br/>");
+
+            foreach ($contact->getMessages() as $message) {
+                $this->flash->error($message);
+            }
+
+            $this->dispatcher->forward([
+                'action' => 'new'
+            ]);
+        }
+
+    }
+
     public function deleteAction($id)
     {
         $contacts = Contacts::findFirst($id);
