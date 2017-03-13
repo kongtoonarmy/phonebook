b36655dd488522b17753b7090fff0ce3520313b9
kongtoonarmy <kongtoonarmy@hotmail.com>
2017-03-13 07:14:36 +0700

    Updating Contacts

--- app/controllers/ContactsController.php
+++ app/controllers/ContactsController.php
@@ -16,6 +16,25 @@ class ContactsController extends Controller {
 
     }
 
+    public function editAction($id)
+    {
+        if (!$this->request->getPost()) {
+            $contact = Contacts::findFirst($id);
+            if (!$contact) {
+                $this->flash->error("Don't try to smart and edit and invalid contact.");
+                $this->dispatcher->forward(['action' => 'index']);
+            } else {
+                $this->tag->displayTo("id", $contact->id);
+                $this->tag->displayTo("name", $contact->name);
+                $this->tag->displayTo("email", $contact->email);
+                $this->tag->displayTo("phone", $contact->phone);
+            }
+        } else {
+            $this->flash->error("Invalid Request!!!");
+            $this->dispatcher->forward(['action' => 'index']);
+        }
+    }
+
     // For creating a new contact in db
     public function createAction()
     {
@@ -37,6 +56,7 @@ class ContactsController extends Controller {
                 'action' => 'index'
             ]);
         } else {
+            $this->flash->setAutomaticHtml(true);
             $this->flash->error("Following Errors occurred: <br/>");
 
             foreach ($contact->getMessages() as $message) {
@@ -48,6 +68,37 @@ class ContactsController extends Controller {
             ]);
         }
 
+    }
+
+    // For updating
+    public function updateAction($id)
+    {
+        if (!$this->request->getPost()) {
+            $this->flash->error("Invalid Request!!!");
+            $this->dispatcher->forward(['action' => 'index']);
+        } else {
+            $id = $this->request->getPost("id");
+            $contact = Contacts::findFirst($id);
+
+            if (!$contact) {
+                $this->flash->error("No such record found");
+                $this->dispatcher->forward(['action' => 'index']);
+            } else {
+                $success = $contact->save($this->request->getPost(), array('name', 'phone', 'email'));
+                if (!$success) {
+                    $this->flash->error("Following Errors occurred: <br>");
+                    foreach ($contact->getMessages() as $message) {
+                        $this->flash->error($message);
+                    }
+                    $this->dispatcher->forword(array(
+                        'action' => 'edit',
+                        'params' => array($contact->id)
+                    ));
+                }
+                $this->flash->success("Contact Successfully Updated!");
+            }
+        }
+        $this->dispatcher->forward(['action' => 'index']);
     }
 
     public function deleteAction($id)
