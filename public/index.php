30201c1bc4d4192eb1dd2180783034f981a83f81
kongtoonarmy <kongtoonarmy@hotmail.com>
2017-03-15 10:00:22 +0700

    Final Touches, Reads ini files

--- public/index.php
+++ public/index.php
@@ -6,13 +6,18 @@ use Phalcon\Mvc\Application;
 use Phalcon\Mvc\View;
 use Phalcon\Mvc\Url as UrlProvider;
 use Phalcon\Db\Adapter\Pdo\Mysql as PdoMysql;
+use Phalcon\Mvc\Router;
+use Phalcon\Config;
+use Phalcon\Config\Adapter\Ini as ConfigIni;
 
 try {
+    $config = new ConfigIni("../app/config/config.ini");
+
     // Register the loader component
     $loader = new Loader();
     $loader->registerDirs([
-        '../app/controllers/',
-        '../app/models/'
+        $config->app->controllersDir,
+        $config->app->modelsDir
     ]);
     $loader->register();
 
@@ -20,28 +25,30 @@ try {
     $di = new FactoryDefault();
 
     // Connect with database
-    $di->set('db', function() {
-       $connect = new PdoMysql([
-           "host" => "localhost",
-           "username" => "root",
-           "password" => "root1234",
-           "dbname" => "phonebook"
-       ]);
+    $di->set('db', function() use ($config) {
+       $connect = new PdoMysql($config->database->toArray());
        return $connect;
     });
 
     // Register the view service
-    $di->set('view', function() {
+    $di->set('view', function() use ($config) {
         $view = new View();
-        $view->setViewsDir('../app/views');
+        $view->setViewsDir($config->app->viewsDir);
         return $view;
     });
 
     // Register the URL service
-    $di->set("url", function () {
-        $url = new UrlProvider();
+    $di->set("url", function () use ($config) {
+        $url = new UrlProvider($config->app->baseUri);
         $url->setBaseUri("/");
         return $url;
+    });
+
+    // Setup the default route
+    $di->set('router', function() use ($config) {
+        $router = new Router();
+        $router->setDefaultController($config->app->setDefaultController);
+        return $router;
     });
 
     // handle requests
